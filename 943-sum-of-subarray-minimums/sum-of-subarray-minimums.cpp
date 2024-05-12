d94e1453c91b4d9a2caea2e63dae977bc1875332
Saad Karim <115962167+Saadkarim402@users.noreply.github.com>
2024-05-12 12:03:42 +0530

    Time: 58 ms (79.71%) | Memory: 41.7 MB (96.93%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,36 +1,22 @@
 class Solution {
-
 public:
-    int sumSubarrayMins(vector<int>& a) {
-        int n=a.size();
-        int m=1e9+7;
-        stack<pair<int,int>>pre,suf;
-        int pre1[n],suf1[n];
-        //suffix
-        for(int i=n-1;i>=0;i--){
-            while(!suf.empty()&&suf.top().first>=a[i]){
-                suf.pop();
-            }
-            if(!suf.empty()) suf1[i]=suf.top().second;
-            else suf1[i]=n;
-            suf.push({a[i],i});
-        }
-        //pre
-        for(int i=0;i<n;i++){
-            while(!pre.empty()&&pre.top().first>a[i]){
-                pre.pop();
-            }
-            if(!pre.empty()) pre1[i]=pre.top().second;
-            else pre1[i]=-1;
-            pre.push({a[i],i});
-        }
+    int sumSubarrayMins(vector<int>& nums) {
+        int n=nums.size();
+        stack<int>st;
         long long ans=0;
-        for(int i=0;i<n;i++){
-            long long l=(suf1[i]-i),r=(i-pre1[i]);
-            long long t=((l%m)*(r%m))%m;
-            long long t1=(t*a[i])%m;
-            ans+=t1%m;
+        int m=1e9+7;
+        for(int i=0;i<=n;i++){
+        while(!st.empty()&&nums[st.top()]>(i==n?-2e9:nums[i])){
+            int j=st.top();
+            st.pop();
+            long long k=(st.empty()?-1:st.top());
+            long long  l=(i-j),r=(j-k);
+            long long h=((l%m)*(r%m))%m;
+            ans+=(nums[j]*h)%m;
+            ans%=m;
         }
-        return ans%m;   
+        st.push(i);
+        }
+        return ans;
     }
 };
