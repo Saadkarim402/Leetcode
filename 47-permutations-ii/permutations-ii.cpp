37b71a32520737b4949c7414092e676fa0ae083e
Saad Karim <115962167+Saadkarim402@users.noreply.github.com>
2024-06-03 18:03:24 +0530

    Time: 5 ms (66.27%) | Memory: 12.1 MB (39.93%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
     public:
-    vector<vector<int>>res;
-    void solve(int st,vector<int>nums){
+    vector<vector<int>>ans;
+    void solve(int st,vector<int>nums,int n){
         //base case
-        if(st==nums.size()){
-            res.push_back(nums);
+        if(st==n){
+            ans.push_back(nums);
             return;
         }
         //st is the starting pt we are changing 
         //we keep it fix 
         //and try to gen all perm after it 
-        for(int i=st;i<nums.size();i++){
+        for(int i=st;i<n;i++){
             if(i==st||nums[i]!=nums[st]){
             swap(nums[i],nums[st]);
-            solve(st+1,nums);
+            solve(st+1,nums,n);
             //swap(nums[i],nums[st]);
             }
         }
@@ -21,9 +21,8 @@ class Solution {
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(nums.begin(),nums.end());
        // ans.clear();
-       int st=0;
-        solve(st,nums);
-        return res;
+        solve(0,nums,nums.size());
+        return ans;
 
         
     }
