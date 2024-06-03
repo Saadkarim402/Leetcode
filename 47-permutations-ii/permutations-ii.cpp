f04161c952196758f320bde48785e8567d5b0263
Saad Karim <115962167+Saadkarim402@users.noreply.github.com>
2024-06-03 17:54:11 +0530

    Time: 3 ms (89.16%) | Memory: 12.1 MB (34.56%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   vector<vector<int>> res;
+    void helper(vector<int> nums, int start){
+        if(start==nums.size()){
+            res.push_back(nums);
+            return;
+        }
+        for(int i=start;i<nums.size();i++){
+            if(i==start || nums[start]!=nums[i]){
+                swap(nums[start],nums[i]);
+                helper(nums,start+1);
+            }
+        }
+        
+    }  
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+         int start=0;
+        sort(nums.begin(), nums.end());
+        helper(nums,start);
+        return res;
+
+        
+    }
+};
