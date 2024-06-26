345ca1c1ef9b3a32ffe142ca522e62974abcb70f
Saad Karim <115962167+Saadkarim402@users.noreply.github.com>
2024-06-26 17:55:14 +0530

    Time: 1505 ms (41.95%) | Memory: 7.7 MB (85.73%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    private:
+    bool pos(int i,int j,string &s){
+        while(i<j){
+            if(s[i]!=s[j])return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+public:
+    int minCut(string s) {
+        int n=s.size();
+        vector<int>dp(n+1,0);
+        
+        for(int i=n-1;i>=0;i--){
+            int mini=1e9;
+            for(int j=i;j<n;j++){
+                if(pos(i,j,s)){
+                    int cost=1+dp[j+1];
+                    mini=min(mini,cost);
+                }
+            }
+            dp[i]=mini;
+        }
+        return dp[0]-1;
+    }
+};
