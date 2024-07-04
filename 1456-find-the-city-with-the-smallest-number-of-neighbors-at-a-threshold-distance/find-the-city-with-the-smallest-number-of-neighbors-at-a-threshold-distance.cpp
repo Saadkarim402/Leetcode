89b866a82ace6f19da84cce8c278ce825d4ad916
Saad Karim <115962167+Saadkarim402@users.noreply.github.com>
2024-07-04 11:34:31 +0530

    Time: 23 ms (80.98%) | Memory: 15.6 MB (62.09%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,48 +1,37 @@
 class Solution {
-    private:
-    vector<int>solve(int i,vector<pair<int,int>>adj[],int n){
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        vector<int>dist(n,1e9);
-        pq.push({0,i});
-        dist[i]=0;
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-            int node=it.second,cost=it.first;
-            for(auto it:adj[node]){
-                int v=it.first,wt=it.second;
-                if(dist[v]>wt+cost){
-                    dist[v]=wt+cost;
-                    pq.push({dist[v],v});
-                }
-            }
-        }
-        return dist;
-    }
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int th) {
-        vector<pair<int,int>>adj[n];
-        for(auto it:edges){
-            int u=it[0],v=it[1],wt=it[2];
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
-        }
-        int cnt_ans=1e9,city;
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            vector<int>dist=solve(i,adj,n);
-            for(int j=0;j<n;j++){
-                if(j!=i&&dist[j]<=th){
-                    cnt++;
-                }
-            }
-            cout<<cnt<<endl;
-            if(cnt_ans>=cnt){
-                cnt_ans=cnt;
-                city=i;
-            }
-        }
-        return city;
-    }
+	int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+		vector<vector<int>> dist(n, vector<int> (n, INT_MAX));
+		for (auto it : edges) {
+			dist[it[0]][it[1]] = it[2];
+			dist[it[1]][it[0]] = it[2];
+		}
+		for (int i = 0; i < n; i++) dist[i][i] = 0;
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
+						continue;
+					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
 
+		int cntCity = n;
+		int cityNo = -1;
+		for (int city = 0; city < n; city++) {
+			int cnt = 0;
+			for (int adjCity = 0; adjCity < n; adjCity++) {
+				if (dist[city][adjCity] <= distanceThreshold)
+					cnt++;
+			}
+
+			if (cnt <= cntCity) {
+				cntCity = cnt;
+				cityNo = city;
+			}
+		}
+		return cityNo;
+
+	}
 };
